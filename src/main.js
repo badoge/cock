e97dba5de2fb2d2eaec6769eff066e4c52fbcf33
badoge <18620902+badoge@users.noreply.github.com>
2024-07-05 23:06:58 +0400

    first release

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 let cocks = 0;
 document.getElementById("cock").addEventListener("click", dank);
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
 function dank() {
   cocks++;
   document.getElementById("cocks").innerHTML = cocks.toLocaleString();
